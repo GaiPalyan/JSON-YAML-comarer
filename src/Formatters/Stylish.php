ec44290d6a7f013c2e374b141d9f55e6c9badfaf
Gai Palyan <ChadReed020788@yandex.ru>
2021-05-23 13:08:39 +0300

    added @param, added typhint for Differ

--- src/Formatters/Stylish.php
+++ src/Formatters/Stylish.php
@@ -2,13 +2,18 @@
 
 namespace Differ\Differ\Stylish;
 
-const IDENT = ' ';
+const INDENT = ' ';
 const INDENTS_SIZE = [
     'STANDARD' => 'standardIdent',
     'BIG' => 'bigIdent'
 ];
 
-function makeStylish($tree, $depth = 1)
+/**
+ * @param array $tree
+ * @param int $depth
+ * @return string
+ */
+function makeStylish(array $tree, int $depth = 1)
 {
     $result = array_map(
         function ($node) use ($depth) {
@@ -19,18 +24,18 @@ function makeStylish($tree, $depth = 1)
                     return  getIndent($depth, INDENTS_SIZE['BIG']) .
                         "{$node['key']}: " . "" . "{$children}";
                 case 'added':
-                    $stringVal = stringify($node['value'], $depth);
+                    $stringVal = stringifyInObjects($node['value'], $depth);
                     return getIndent($depth) . "+ {$node['key']}: {$stringVal}";
                 case 'delete':
-                    $stringVal = stringify($node['value'], $depth);
+                    $stringVal = stringifyInObjects($node['value'], $depth);
                     return getIndent($depth) . "- {$node['key']}: {$stringVal}";
                 case 'unchanged':
-                    $stringVal = stringify($node['value'], $depth);
+                    $stringVal = stringifyInObjects($node['value'], $depth);
                     return getIndent($depth) . "  {$node['key']}: {$stringVal}";
                 case 'modified':
                     $newLine = getIndent($depth);
-                    $oldValue = stringify($node['before'], $depth);
-                    $newValue = stringify($node['after'], $depth);
+                    $oldValue = stringifyInObjects($node['before'], $depth);
+                    $newValue = stringifyInObjects($node['after'], $depth);
                     return "{$newLine}- {$node['key']}: {$oldValue}\n"
                             . "{$newLine}+ {$node['key']}: {$newValue}";
                 default:
@@ -43,14 +48,14 @@ function makeStylish($tree, $depth = 1)
     return "{\n{$result}\n" . getIndent($depth - 1, INDENTS_SIZE['BIG']) . "}";
 }
 
-function stringify($value, $depth = 1)
+function stringifyInObjects($value, int $depth = 1): string
 {
     if (!is_object($value)) {
         return toString($value);
     }
     $result = array_map(
         function ($key) use ($value, $depth) {
-            $stringifier = stringify($value->$key, $depth + 1);
+            $stringifier = stringifyInObjects($value->$key, $depth + 1);
             return getIndent($depth + 1, INDENTS_SIZE['BIG']) . "{$key}: {$stringifier}";
         },
         array_keys((array) $value)
@@ -58,20 +63,25 @@ function stringify($value, $depth = 1)
     return "{\n" . implode("\n", $result) . "\n" . getIndent($depth, INDENTS_SIZE['BIG']) . "}";
 }
 
-function getIndent($depth, $identType = 'standardIdent')
+/**
+ * @param int $depth
+ * @param string $identType
+ * @return string
+ */
+function getIndent(int $depth, string $identType = 'standardIdent'): string
 {
 
     switch ($identType) {
         case 'standardIdent':
-            return  str_repeat(IDENT, $depth * 4 - 2);
+            return  str_repeat(INDENT, $depth * 4 - 2);
         case 'bigIdent':
-            return str_repeat(IDENT, $depth * 4);
+            return str_repeat(INDENT, $depth * 4);
         default:
             throw new \Error("wrong ident type $identType");
     }
 }
 
-function toString($value)
+function toString($value): string
 {
     if (is_bool($value)) {
         return $value ? 'true' : 'false';
